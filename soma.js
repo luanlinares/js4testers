52858a759d2f0d11022d48d0dd54d2f099d7c087
Luan Linares <luan.miguel@rarolabs.com.br>
2020-04-16 16:35:32 -0300

    Finish

--- soma.js
+++ soma.js
@@ -9,30 +9,4 @@ function somaValores() {
     divResultado.append("O resultado da soma é: " + result);
     
     console.log(result)
-}       
-
-//DOM - Document Object Mode
-//Representa as marcações do HTML lidas pelo navegador e exibidas para o usuario.
-
-//Estrutura do DOM
-// html => Página
-// h1 => Cabeçalhos ou titulos
-// p => paragrafos
-// ul => Agrupadores de Listas
-// li => itens de listas do elemento ul
-// a => link 
-//form => formularios
-//input => campos para inserir informações de diversos tipos:
-    //texto => Campo para digitação de texto
-    //check => checkbox
-    //radio => botões de radio
-
-//textarea => area de texto (paragrafos maiores)
-//button => botoes
-//spam => elementos genericos
-//tables => Tabelas
-//tds => Colunas
-//divs => Divisões
-//labels => textos 
-
-//=============================================================================
+}
